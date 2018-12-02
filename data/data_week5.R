a768220bc68cd509ddc09ed9611ff59eda2e58c1
akaariala <44606709+akaariala@users.noreply.github.com>
2018-12-02 23:49:20 +0200

    Week 5

---
+++ data/data_week5.R
@@ -0,0 +1,34 @@
+# Antti Kääriälä
+# 24 Nov 2018
+# Data wrangling, week 5
+
+setwd("~/GitHub/IODS-project/data")
+
+library(tidyverse)
+
+# reading data
+human <- readRDS("human.rds")
+
+# keep these variables
+keep <- c("country", "edu_sex_ratio", "labor_sex_ratio",
+          "exp_education", "life_expectancy", "gni",
+          "maternal_mortality", "teen_births", "repr_w")
+
+# keep these rows
+last <- nrow(human) - 7
+
+# data manipulation
+human_ 
+human %>%
+  mutate(gni = str_replace(gni, pattern=",", replacement =""),
+         gni = as.numeric(gni)) %>%
+  select(one_of(keep)) %>%
+  filter(between(row_number(), 1, last)) %>%
+  drop_na() %>% 
+  as.data.frame() %>%
+  column_to_rownames(var = "country") 
+
+# daving data
+write_rds(human_, "human.rds")
+
+
